119d26d5b3556e6abc7c7f0e561194bbb40ff65f
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-17 21:28:19 -0500

    number function

--- criddle_jeremy_assignment2/criddle_jeremy_assignment2.js
+++ criddle_jeremy_assignment2/criddle_jeremy_assignment2.js
@@ -11,7 +11,7 @@ var name = "S107",
 //functions
 var isCharged = function (battery) {
     if (battery > 20) {
-        console.log("Check, we have " + battery + "% battery")
+        console.log("We have " + battery + "% battery, we are a go!")
     } else {
         console.log("The battery is low, please charge before flight.")
     }
@@ -19,8 +19,24 @@ var isCharged = function (battery) {
 var conditions;
 conditions = function (windSpeed, windTolerence) {
     if (windSpeed < windTolerence) {
-        console.log('Wind speed check is a go.");
+        console.log("Wind speed check is a go.");
+        return
     }
     else ("Perhaps you should wait for better wind conditions.");
+    return
 };
+var startRotor = function (rotorSpeed) {
+    while (rotorspeed <= 60);
+    console.log("Windspeed is at " + rotorSpeed + "percent");
+    rotorspeed = rotorSpeed + 5;
+}
+
+
+console.log("Starting pre-flight check list");
+console.log('First up is the battery check.');
+isCharged(99);
+console.log('Next up, lets check the wind conditions.')
+var currentConditions = conditions(2, 5);
+console.log("Roger, lets go ahead and get the main rotor up to speed");
+var rotorSpeed = startRotor(0);
 
