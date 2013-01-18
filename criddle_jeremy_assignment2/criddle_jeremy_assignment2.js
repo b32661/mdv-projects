8fa0fb19879a7870933fb85013b02d4cf64dab79
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-17 19:15:05 -0500

    framework

--- criddle_jeremy_assignment2/criddle_jeremy_assignment2.js
+++ criddle_jeremy_assignment2/criddle_jeremy_assignment2.js
@@ -1,4 +1,34 @@
 // Jeremy Criddle
-// Tuesday, January 8th 2013
+// SDI 1301
 // Project 2
 // theme
+
+//set global variables
+
+//variables
+var mystring = "",
+    mynumber = 20,
+    myarray = ["Apprentice", "Journeyman", "Master"];
+
+//functions
+var myprocedure = function () {
+
+};
+var myboolean = function(){
+
+};
+var mynumber1 = function(){
+
+};
+var mystring1 = function(){
+
+};
+
+var myarray1 = function(){
+
+};
+returned values
+
+
+myProcedure(5);
+console.log(log values);
