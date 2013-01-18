87e51b8b0496917f6510406bcc81feed21b4e09f
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-17 22:10:37 -0500

    math function

--- criddle_jeremy_assignment2/criddle_jeremy_assignment2.js
+++ criddle_jeremy_assignment2/criddle_jeremy_assignment2.js
@@ -1,11 +1,11 @@
 // Jeremy Criddle
 // SDI 1301
 // Project 2
-// R/C Helicopter
+// R/C Helicopter pre-flight checklist
 
 //set global variables
 var name = "S107",
-    numberOfFlights = 16,
+    flights = 6,
     controls = ["more throttle", "less throttle", "forward", "backwards", "rotate left", "rotate right"];
 
 //functions
@@ -26,17 +26,25 @@ conditions = function (windSpeed, windTolerence) {
     return
 };
 var startRotor = function (rotorSpeed) {
-    while (rotorspeed <= 60);
-    console.log("Windspeed is at " + rotorSpeed + "percent");
-    rotorspeed = rotorSpeed + 5;
-}
+    while (rotorSpeed <= 80){
+        console.log("Our main rotor is at " + rotorSpeed + " % power, looking good!");
+        rotorSpeed += 10;
+    } return rotorSpeed;
+};
+var myString = function (){
 
+};
+var
 
+console.log("model " + name + " infrared helicopter, with " + flights + " successful flights completed, ready for pre-flight check.");
 console.log("Starting pre-flight check list");
 console.log('First up is the battery check.');
 isCharged(99);
 console.log('Next up, lets check the wind conditions.')
 var currentConditions = conditions(2, 5);
-console.log("Roger, lets go ahead and get the main rotor up to speed");
-var rotorSpeed = startRotor(0);
+console.log("Roger, lets go ahead and get the main rotor up to speed.");
+var mainRotorSpeed = startRotor(0);
+console.log("Fantastic, main rotor is good, let's reduce power back down to 40%.");
+mainRotorSpeed = 40;
+console.log(mainRotorSpeed + " %, check.");
 
